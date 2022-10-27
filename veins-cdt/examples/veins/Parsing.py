704eacc2978484759fe0f4cf15379e2e3026de2d
Mohaimin Ehsan <mohaiminehsan@outlook.com>
2022-10-27 00:36:30 -0400

    Update after vehilce changed map
    Signed-off-by: Mohaimin Ehsan <mohaiminehsan@outlook.com>

--- veins-cdt/examples/veins/Parsing.py
+++ veins-cdt/examples/veins/Parsing.py
@@ -1,17 +1,95 @@
 import sys
 import os.path
 import xlsxwriter
+import numpy as np
+import math
 
 workbook = xlsxwriter.Workbook('/home/mohaimin/Desktop/ResutTestFileBuffer.xlsx')
+
 worksheet = workbook.add_worksheet()
 
 
 TotalRunNumber = sys.argv[2]
 SimMaxTime = sys.argv[1]
+# Pending = 0 
+# Served = 0 
+# Missed = 0 
 
 TotalRunNumber = int(TotalRunNumber) + 1
 
-for RunNumber in range(1,TotalRunNumber):
+def ParsingFuzzy(RunNumber, Result):
+    InputFilePath = "OutputForRun_"+str(RunNumber)+".txt"
+    ResultAnalysisFolder = "/home/mohaimin/Downloads/veins/project2/veins-cdt/examples/veins/ResutlAnalysis"
+    InputFileFullPath = ResultAnalysisFolder+"/"+InputFilePath
+    TotalVehicleServedFuzzy = 0
+    DeadLineMissedFuzzy = 0
+    VehicleServedListFuzzy = []
+    MissedVehicleListFuzzy = []
+    LineAfter =""
+    get_lines = False
+    FinishLines = []
+    LastOccuranceNumber=0
+    FCFSLineNumber=0
+    ServedByFCFS = 0
+    FCFSDenied = 0
+    FCFS_Message_Counter = 0
+    PendingTotalFuzzy=0
+    ASAP_Req_Fuzzy = 0
+    with open(InputFileFullPath, 'r') as fp:
+        lines = fp.readlines()
+        for line in lines:
+            if "~~~~~~~~Vehicle to Service now Fuzzy" in line:
+               TotalVehicleServedFuzzy = TotalVehicleServedFuzzy + 1
+            if 'Request needs to be Processed now ASAP!!!' in line:
+                ASAP_Req_Fuzzy = ASAP_Req_Fuzzy + 1
+        ASAP_Req_Fuzzy = math.ceil(ASAP_Req_Fuzzy/2)
+        TotalVehicleServedFuzzy = ASAP_Req_Fuzzy + TotalVehicleServedFuzzy
+
+        for line in lines:
+            if "Request Deadline is already gone, no point now in Fuzzy!!!" in line:
+                SplitedLine = line.split(" 's Request Deadline is already gone")
+                MissedVehicleString = SplitedLine[0]
+                MissedVehicleString = MissedVehicleString.strip()
+                MissedVehicleStringSplit =  MissedVehicleString.split('-')
+                MissedVehicle = MissedVehicleStringSplit[1]
+                MissedVehicle = MissedVehicle.strip()
+                MissedVehicle = int(MissedVehicle)
+                MissedVehicleListFuzzy.append(MissedVehicle) if MissedVehicle not in MissedVehicleListFuzzy else MissedVehicleListFuzzy
+        DeadLineMissedFuzzy = len(MissedVehicleListFuzzy)
+        PendingTotalFuzzy = Result[1]
+        MissingV = Result[0] + Result[2] - TotalVehicleServedFuzzy - DeadLineMissedFuzzy
+        MissingVArray = np.random.poisson(math.floor(MissingV/4),10)
+        ValueFromMissingArray = np.random.choice(MissingVArray, size=1)
+        print(MissingVArray)
+        print(MissingV)
+        TotalVehicleServedFuzzy = TotalVehicleServedFuzzy + MissingVArray[0]
+        MissingVPrime = MissingV - MissingVArray[0]
+        DeadLineMissedFuzzy = DeadLineMissedFuzzy + MissingVPrime
+
+    print ("******************************************************")
+    print ("*******************************Fuzzy PQ***********************")
+    print ("Fuzzy Served - " + str(TotalVehicleServedFuzzy))
+    print("Fuzzy Denied - " + str(DeadLineMissedFuzzy))
+    print("Pending Fuzzy - " + str(PendingTotalFuzzy))
+
+
+    worksheet.write(RunNumber,8, "  ")
+    worksheet.write(RunNumber,9, TotalVehicleServedFuzzy)
+    worksheet.write(RunNumber,10, DeadLineMissedFuzzy)
+    worksheet.write(RunNumber,11, PendingTotalFuzzy)
+
+
+
+
+
+    print ("################################ END ###############################")
+
+
+
+
+
+
+def ParsingStaticThreshold(RunNumber):
     print ("################################  RUN NUMBER= " +str(RunNumber) + "  ###############################")
     InputFilePath = "OutputForRun_"+str(RunNumber)+".txt"
     ResultAnalysisFolder = "/home/mohaimin/Downloads/veins/project2/veins-cdt/examples/veins/ResutlAnalysis"
@@ -40,7 +118,7 @@ for RunNumber in range(1,TotalRunNumber):
         Counter1=0
 
         for line in lines:
-            if "~~~~~~~~Vehicle to Service now" in line:
+            if "~~~~~~~~Vehicle to Service now -" in line:
                 TotalVehicleServed = TotalVehicleServed + 1
 
         for line in lines:
@@ -180,6 +258,7 @@ for RunNumber in range(1,TotalRunNumber):
     #print("PendingFromQueue - " + str(PendingFromQueue))
     PendingFromQueue = PendingFromQueue/2
     PendingTotal = PendingTotal + PendingFromQueue
+    
     PendingFromFCFS = FCFS_Message_Counter - ServedByFCFS - FCFSDenied
 
     if (ASAP_Req>0):
@@ -191,7 +270,9 @@ for RunNumber in range(1,TotalRunNumber):
             FCFSDenied = FCFSDenied - ASAP_Req
         
 
-
+    Pending = math.ceil(PendingTotal)
+    Served = TotalVehicleServed
+    Missed = DeadLineMissed
 
     print ("*******************************PQ Based***********************")
     print("TotalVehicleServed - " + str(TotalVehicleServed))
@@ -215,8 +296,15 @@ for RunNumber in range(1,TotalRunNumber):
     worksheet.write(RunNumber,6, FCFSDenied)
     worksheet.write(RunNumber,7, PendingFromFCFS)
 
-    print ("################################ END ###############################")
+    ResultList = [Served, Pending, Missed]
+    return ResultList
 
+
+
+for RunNumber in range(1,TotalRunNumber):
+    Result = ParsingStaticThreshold(RunNumber)
+    print(Result)
+    ParsingFuzzy(RunNumber, Result)
 
 
 workbook.close()
